dfa934a0fcb4f67dbc47eb405e5696242566e169
Venkat Peri <venkatperi@gmail.com>
2018-02-03 15:40:44 -0500

    nginx

--- zsh/nginx_aliases.zsh
+++ zsh/nginx_aliases.zsh
@@ -1,3 +1,6 @@
-alias ngstop="sudo kill -QUIT $( cat /var/run/nginx.pid )"
-alias ngreload="sudo kill -HUP $( cat /var/run/nginx.pid )"
-alias ngtest="sudo nginx -c /etc/nginx/nginx.conf -t"
+if [ -f /var/run/nginx.pid ]
+then
+  alias ngstop="sudo kill -QUIT $( cat /var/run/nginx.pid )"
+  alias ngreload="sudo kill -HUP $( cat /var/run/nginx.pid )"
+  alias ngtest="sudo nginx -c /etc/nginx/nginx.conf -t"
+fi
