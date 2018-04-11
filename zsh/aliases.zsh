5b0b67cd8f31c6ad3819e9e365d107b24e2b52a9
Venkat Peri <venkatperi@gmail.com>
2018-04-11 10:54:07 -0400

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,12 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-alias ls='ls --color=auto'
 alias gs='git status'
 alias gac='git add . && git commit -m '
-alias gpom='git push origin master'
+alias gpom='git push origin master --tags'
+
+alias gwb='./gradlew build'
+alias gwcb='./gradlew clean build'
+alias gw='./gradlew'
 
 alias nis='npm install --save'
---
+++ zsh/android_path.zsh
@@ -0,0 +1,4 @@
+export ANDROID_SDK_ROOT=/Users/venkat/Library/Android/sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+export PATH=$PATH:$ANDROID_SDK_ROOT/tools
+export ANDROID_SDK_HOME=/Users/venkat
