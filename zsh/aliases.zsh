3af1a1832158f003d79cf49ed1a2488a0f4ed73f
Venkat Peri <venkatperi@gmail.com>
2019-11-25 08:12:45 -0800

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,6 @@ alias gwb='./gradlew build'
 alias gwcb='./gradlew clean build'
 alias gw='./gradlew'
 alias lc='colorls -lA --sd'
+alias ls=exa
+alias cat=bat
 
--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,5 +1,8 @@
 alias gs='git status'
 alias gac='git add . && git commit -m '
 alias gpom='git push origin master --tags'
+alias guo='git pull origin '
+alias gpo='git push origin '
+alias gc='git checkout '
 alias glog="git log --pretty=format:\"%h - %an, %ar : %s\""
 
